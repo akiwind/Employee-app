13de16314d3e65badbd00ef4a552ae4cb66accb4
Weronika Szotowicz <weronikaszotowicz@gmail.com>
2025-11-05 16:19:19 +0100

    added db and env

--- .env.py
+++
@@ -1,6 +0,0 @@
-
-
-DB_HOST=localhost
-DB_NAME=employees_db
-DB_USER=postgres
-DB_PASSWORD=1996
--- db.py
+++ db.py
@@ -1,11 +1,15 @@
+import os
+from dotenv import load_dotenv
 import psycopg2
+
+load_dotenv()
 
 def get_conn():
     conn = psycopg2.connect(
-        database= 'employees_db',
-        user='postgres',
-        password='',
-        host='localhost',
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
     return conn
 
